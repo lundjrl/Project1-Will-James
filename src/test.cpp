8ef693acf9fc0662b563bb97a022637d7df104f7
William Shreeve <shreevew@eos15.cis.gvsu.edu>
2018-02-02 16:32:29 -0500

    Fixed and added correct testing cases

--- src/test.cpp
+++ src/test.cpp
@@ -1,5 +1,18 @@
 #include "../include/AUDS.h"
-
+#include <string>
+#include <iostream>
 int main(int argc, char** argv){
+	AUDS<int> dataStruct;
+	for(int i = 0; i <= 99; i++){
+		dataStruct.push(i);
+	}
+	dataStruct.printData();
+	int popped = dataStruct.pop();
+	std::cout << std:: endl << popped << std::endl;
+	dataStruct.printData();
+	dataStruct.push(100);
+	dataStruct.push(101);
+	dataStruct.printData();
+	std::cout << std:: endl << popped << std::endl;	
 	
 }
